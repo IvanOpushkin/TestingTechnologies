f52f0e897cec483d06ed6dd364e94d79843ddcc9
IvanOpushkin <farm.vanya@yandex.ru>
2018-02-13 23:06:09 +0900

    Version controls

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,6 +1,9 @@
 DROP TABLE IF EXISTS user_gays;
 
+--TRUNCATE meals;
+--INSERT INTO public.meals (id, user_id, date_time, description, calories, cod, naimenovanie, type1, type2, proizvoditel, edizmereniya, kolvo, cena, primechanie, articul, picture, polka) VALUES (301355, 100000, '2018-02-01 23:46:20.747000', 'o', 'o', '18397', 'Клемма EkinoxeNX ноль 30', 'Электромонтажное ', '', 'legrand', 'шт', 13, 500, '', '601293', '18397osnova', '1A1');
 
+--INSERT INTO public.meals (id, user_id, date_time, description, calories, cod, naimenovanie, type1, type2, proizvoditel, edizmereniya, kolvo, cena, primechanie, articul, picture, polka) VALUES (100708, 200001, '2018-01-16 06:31:07.846000', 'o', 'o', '521', 'Кабель ВВГнг-LS 5х50', 'Кабельная продукция', '', '', 'Метры', 400, 1254, 'Космодром', '', '521osnova', 'Уличная площадка 2');
 
 --update meals set cod = '0' || cod WHERE description='ричи';
 
